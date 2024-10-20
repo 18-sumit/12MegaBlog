97e8b08b6f59472d23ba21ea5866bcecd0bf8706
Sumit Singh <sumitsingh3076@gmail.com>
2024-10-20 18:22:30 +0530

    loginBtn

--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,13 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Header/Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
 
 export {
     Header,
     Footer,
     Container,
-    Logo
+    Logo,
+    LogoutBtn
 }
