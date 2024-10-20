88d1472a4fc6aca9e0dd3a03499d28ec9011e6e3
Sumit Singh <sumitsingh3076@gmail.com>
2024-10-20 18:21:49 +0530

    ..

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,20 @@
-import React, { Children } from 'react'
+// It is a common button can  be used anywhere needed
+import React from "react";
 
+//children is just a fancy word , doesn't mean anything it can also be named as text , or anything else 
 function Button({
-    Children,
+    children,
     type = 'button',
-    bgcolor = 'bg-blue-600',
+    bgColor = 'bg-blue-600',
     textColor = 'text-white',
-    className = '',
+    className = '', // empty because , what if user have to pass any className then from here we can take it 
     ...props
+    // this is props , if user have passed any other properties like style , value , id or anything  else then it will be here 
+    // and then using spread operator all those properties will be here 
+
 }) {
     return (
-        <button className={`px-4 py-2 rounded-lg ${bgcolor} ${textColor} ${className}`} {...props}>{Children}</button>
+        <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>{children}</button>
     )
 }
 
