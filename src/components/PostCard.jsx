da23f2a11f1ca5581d5bdbb19ddd9b4f2ffd1147
Sumit Singh <sumitsingh3076@gmail.com>
2024-10-24 19:58:18 +0530

    why $id  is used .

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,7 +3,9 @@ import appwriteService from '../appwrite/config'
 import { Link } from 'react-router-dom'
 
 
-function PostCard({$id , title , featuredImage}) {
+function PostCard({$id , title , featuredImage}) { 
+  // since we are using appwrite , they have syntax of writing id in $id
+  
   return (
     <Link to ={`/post/${$id}`}> // to means href here
         <div className='w-full bg-gray-100 rounded-xl p-4'>
