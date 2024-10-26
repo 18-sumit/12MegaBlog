b59532ec4a9768b1b7eba6f60e33dab459ceab09
Sumit Singh <sumitsingh3076@gmail.com>
2024-10-26 18:35:43 +0530

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,74 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { Children, StrictMode } from 'react'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { AuthLayout, Post } from './components/index.js'
+import { RouterProvider } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    Children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/login',
+        element: (
+          <AuthLayout authentication={false}> // to make sure that we don't bypass the routes manually even without login
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/signup',
+        element: (
+          <AuthLayout authentication={false} >
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/all-post',
+        element: (
+          <AuthLayout authentication >
+            <AllPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/addpost',
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AddPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/edit-post/:slug',
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/post/:slug',
+        element: <Post />,
+      },
+    ],
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <RouterProvider  router={router} />
     </Provider>
-  </StrictMode>,
+  </React.StrictMode>,
 )
