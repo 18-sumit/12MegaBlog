08c2034c85e1fef51e4303e09bdd50c6e89e3042
Sumit Singh <sumitsingh3076@gmail.com>
2024-10-28 18:48:44 +0530

    ..

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,30 +1,27 @@
-import { React, useId } from 'react'
+import React, { useId } from 'react'
 
 function Select({
-    options, // mostly ek aaray hee milta h
+    options,
     label,
-    className= '',
+    className,
     ...props
-
 }, ref) {
     const id = useId()
     return (
-
         <div className='w-full'>
             {label && <label htmlFor={id} className=''></label>}
-            <select {...props}
-            id={id}
-            ref={ref}
-            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+            <select
+                {...props}
+                id={id}
+                ref={ref}
+                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
             >
-                // hence we map  the options array
-
+                // hence we map the options array
                 {options?.map((option) => (
                     <option key={option} value={option}>
                         {option}
                     </option>
                 ))}
-
             </select>
         </div>
     )
