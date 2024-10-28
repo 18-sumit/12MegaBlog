fb295e2830afa3305b5471b552bca8358011577e
Sumit Singh <sumitsingh3076@gmail.com>
2024-10-28 18:41:59 +0530

    outlet from react-dom error

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { useDispatch } from 'react-redux'
 import './App.css'
 import authService from './appwrite/auth';
 import { login, logout } from './store/authSlice'
-import {Header , Footer } from './components'
-// import { Outlet } from 'react-router-dom';
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom';
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -22,18 +22,18 @@ function App() {
       })
       .finally(() => setLoading(false))
   }, [])
-  
+
 
   return !loading ? (
-    <div className = 'min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
 
       <div className='w-full block'>
         <Header />
         <main>
-         TODO: {/* <Outlet /> */}
+          <Outlet />
         </main>
         <Footer />
-        
+
       </div>
 
     </div>
