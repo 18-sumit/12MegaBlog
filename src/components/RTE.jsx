626d54c3d891ce604c789ce74371213323ddeeab
Sumit Singh <sumitsingh3076@gmail.com>
2024-11-01 16:55:43 +0530

    api error

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -17,7 +17,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
                 control={control}
                 render={({ field: { onChange } }) => (
                     <Editor
-                        apiKey={import.meta.env.VITE_APPWRITE_TINYMCE_API}
+                        apiKey="q7r28ed4rhsh7xz4pkdcqjt43nsywby8b69pvigahn7o52tl"
                         initialValue={defaultValue}
                         init={{
                             initialValue: defaultValue,
